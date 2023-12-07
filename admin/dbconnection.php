3cd101dd40c32bba197b313bfb3ce74df8068ede
SatyaSaiKarthik <113677916+SatyaSaiKarthik@users.noreply.github.com>
2023-12-07 13:52:28 +0530

    Update dbconnection.php

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "crm");
+$con=mysqli_connect("cmr.mysql.database.azure.com", "cloud", "Azure1234567890", "cmr");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
